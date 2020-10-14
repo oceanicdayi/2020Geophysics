bb846a3ad94ddfa8efd5d52feb101ebb929f5c19
oceanicdayi <oceanicdayi@gmail.com>
2020-10-14 09:19:32 +0800

    1014 modified

--- GMT_homework-1.bat
+++
@@ -1,3 +0,0 @@
-gmt begin GMT_tut_1
-	gmt basemap -R10/70/-3/8 -JX2i/8i -B -B+glightred+t"My first plot"
-gmt end show
--- gmt_homework2.bat
+++
@@ -1,4 +0,0 @@
-gmt begin GMT_2 
-   gmt coast -R119/123/21/26 -JM10 -B1 -Df -G100 
-   gmt plot data.txt -Sc0.8 -Gred 
-gmt end show 
